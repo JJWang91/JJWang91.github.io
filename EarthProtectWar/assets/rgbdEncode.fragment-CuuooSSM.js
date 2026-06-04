628a8ef3c6fe79579ae456534787344768494d98
Hiwen <twgwwg@foxmail.com>
2026-06-05 00:18:23 +0800

    EarthProtectWar

---
+++ EarthProtectWar/assets/rgbdEncode.fragment-CuuooSSM.js
@@ -0,0 +1,5 @@
+import{b5 as e}from"./index-B3SpG6DQ.js";import{h as t}from"./helperFunctions-CqIykJeo.js";const o="rgbdEncodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=toRGBD(texture2D(textureSampler,vUV).rgb);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const a=[t];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const i={name:o,shader:n};export{i as rgbdEncodePixelShader};
