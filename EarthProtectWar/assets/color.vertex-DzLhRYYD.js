fc36231225d66016d25a6832101f58025fc3f8b0
Hiwen <twgwwg@foxmail.com>
2026-06-07 00:05:41 +0800

    update

---
+++ EarthProtectWar/assets/color.vertex-DzLhRYYD.js
@@ -0,0 +1,32 @@
+import{b5 as e}from"./index-B993nObd.js";import{c as t,b as o,e as a,i as c,g as d,d as s,a as l,f}from"./clipPlaneVertex-B7mmew8F.js";import{f as S,a as x,v as u}from"./vertexColorMixing-BZ0p1esY.js";const i="colorVertexShader",r=`attribute position: vec3f;
+#ifdef VERTEXCOLOR
+attribute color: vec4f;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<clipPlaneVertexDeclaration>
+#include<fogVertexDeclaration>
+#ifdef FOG
+uniform view: mat4x4f;
+#endif
+#include<instancesDeclaration>
+uniform viewProjection: mat4x4f;
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+varying vColor: vec4f;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+@vertex
+fn main(input : VertexInputs)->FragmentInputs {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#ifdef VERTEXCOLOR
+var colorUpdated: vec4f=vertexInputs.color;
+#endif
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+var worldPos: vec4f=finalWorld* vec4f(vertexInputs.position,1.0);vertexOutputs.position=uniforms.viewProjection*worldPos;
+#include<clipPlaneVertex>
+#include<fogVertex>
+#include<vertexColorMixing>
+#define CUSTOM_VERTEX_MAIN_END
+}`;e.ShadersStoreWGSL[i]||(e.ShadersStoreWGSL[i]=r);const V=[t,o,a,S,c,d,s,l,f,x,u];for(const n of V)e.IncludesShadersStoreWGSL[n.name]||(e.IncludesShadersStoreWGSL[n.name]=n.shader);const p={name:i,shader:r};export{p as colorVertexShaderWGSL};
