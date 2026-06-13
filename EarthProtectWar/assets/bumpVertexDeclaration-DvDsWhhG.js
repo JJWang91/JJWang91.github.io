b9e3619ea53f18859a7c15b977cb3e6e9cbcb7b8
Hiwen <twgwwg@foxmail.com>
2026-06-13 22:43:25 +0800

    update

---
+++ EarthProtectWar/assets/bumpVertexDeclaration-DvDsWhhG.js
@@ -0,0 +1,6 @@
+import{b5 as d}from"./index-B3UrsgLD.js";const e="bumpVertexDeclaration",n=`#if defined(BUMP) || defined(PARALLAX) || defined(CLEARCOAT_BUMP) || defined(ANISOTROPIC)
+#if defined(TANGENT) && defined(NORMAL) 
+varying mat3 vTBN;
+#endif
+#endif
+`;d.IncludesShadersStore[e]||(d.IncludesShadersStore[e]=n);const r={name:e,shader:n};export{r as b};
--- EarthProtectWar/assets/kernelBlurVaryingDeclaration-BWALZG-7.js
+++
@@ -1,1 +0,0 @@
-import{b5 as e}from"./index-BE-hTyuV.js";const r="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=a);const o={name:r,shader:a};export{o as k};
