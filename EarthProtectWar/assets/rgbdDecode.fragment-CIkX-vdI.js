1630f2c6042db908e89968c819a3f07296aeecc6
Hiwen <twgwwg@foxmail.com>
2026-06-15 23:20:19 +0800

    update

---
+++ EarthProtectWar/assets/rgbdDecode.fragment-CIkX-vdI.js
@@ -0,0 +1,5 @@
+import{b5 as e}from"./index-CeO2O4d3.js";import{h as t}from"./helperFunctions-BZfh1_PO.js";const o="rgbdDecodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=vec4(fromRGBD(texture2D(textureSampler,vUV)),1.0);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const a=[t];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const i={name:o,shader:n};export{i as rgbdDecodePixelShader};
--- EarthProtectWar/assets/rgbdEncode.fragment-DMuqIkXo.js
+++
@@ -1,5 +0,0 @@
-import{b5 as e}from"./index-Bangx7Fm.js";import{h as t}from"./helperFunctions-BB1Ct_I1.js";const o="rgbdEncodePixelShader",n=`varying vec2 vUV;uniform sampler2D textureSampler;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-void main(void) 
-{gl_FragColor=toRGBD(texture2D(textureSampler,vUV).rgb);}`;e.ShadersStore[o]||(e.ShadersStore[o]=n);const a=[t];for(const r of a)e.IncludesShadersStore[r.name]||(e.IncludesShadersStore[r.name]=r.shader);const i={name:o,shader:n};export{i as rgbdEncodePixelShader};
